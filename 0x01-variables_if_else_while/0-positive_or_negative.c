04c8c0b0d4ba2303bb36cf27a51a19a961a09ce1
Viv <kendivivianne@gmail.com>
2022-09-10 09:34:20 -0700

    If else statement with random numbers

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -5,7 +5,7 @@
  * main - assigns a random number to int n everytime
  * it executes, and prints it
  * Return: Always 0 (Success)
- * betty style doc for functionmain goes there 
+ * betty style doc for function main goes there
  */
 int main(void)
 {
