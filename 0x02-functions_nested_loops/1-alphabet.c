5645eebad63563e61cb043210d494daf514f583e
Viv <kendivivianne@gmail.com>
2022-09-13 12:37:28 -0700

    printing the alphabet

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * main - prints the alphabet in lowercase
+ * followed by a new line
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	void(print_alphabet(void)
+		{
+			int c;
+
+			while(c >= 'a' && c <= 'z')
+			{
+				_putchar(c);
+				c++;
+			}
+		}
+		return (0);
+}
