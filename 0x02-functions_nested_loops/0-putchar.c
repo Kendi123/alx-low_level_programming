fc3c63f355ba843703413cb4b7892776c1af3ac9
Viv <kendivivianne@gmail.com>
2022-09-14 13:11:21 -0700

    printing _putchar

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -7,10 +7,14 @@
  */
 int main(void)
 {
-	int _putchar(void)
-	{
-		return (write(1, _putchar, 8));
-	}
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
+	_putchar('\n');
 	return (0);
 }
-
