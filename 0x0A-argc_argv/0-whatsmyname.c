4d1661f7aced45abbc9f2be4dcb845c598b209a3
Viv <kendivivianne@gmail.com>
2022-10-02 05:15:27 -0700

    printing the program name

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - the start of the program
+ * @argc: argument count
+ * @argv: argument vector
+ * Return: Always Success (0)
+ */
+int main(int argc, int  **argv)
+{
+	printf("%s\n", argv[0]);
+	return (0);
+}
