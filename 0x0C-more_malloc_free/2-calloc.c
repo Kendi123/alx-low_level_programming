953b629071f219dd2be2193ea025fe0d1326cf2d
Viv <kendivivianne@gmail.com>
2022-10-06 13:08:33 -0700

    first changes

---
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -0,0 +1,12 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * _calloc - allocates memory for an array, using malloc.
+ * @nmemb: number of elements in the array
+ * @size: size of byte
+ * Return: a pointer to the allocated memory.
+ */
+void *_calloc(unsigned int nmemb, unsigned int size)
+{
+}
---
+++ 0x0C-more_malloc_free/3-array_range.c
@@ -0,0 +1,12 @@
+#include "main.h"
+#include <stdlib.h>
+#include <stdio.h>
+/**
+ * array_range - creates an array of integers.
+ * @min: minimum value
+ * @max: maximum value
+ * Return: the pointer to the newly created array
+ */
+int *array_range(int min, int max)
+{
+}
