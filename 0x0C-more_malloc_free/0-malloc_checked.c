0318b911ac7108aaf53cdc24cd76bb254949dcff
Viv <kendivivianne@gmail.com>
2022-10-06 13:01:21 -0700

    first changes

---
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -0,0 +1,12 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib>
+/**
+ * malloc_checked - allocates memory using malloc.
+ * @b: parameter
+ * Return: a pointer to the allocated memory if successful, if not,
+ * a normal process termination with a status value of 98
+ */
+void *malloc_checked(unsigned int b)
+{
+}
---
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -0,0 +1,12 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * string_nconcat - concatenates two strings.
+ * @s1: string 1
+ * @s2: string 2@n: parameter
+ * Return: a pointer to a newly alloacted space in memory
+ */
+char *string_nconcat(char *s1, char *s2, unsigned int n)
+{
+}
