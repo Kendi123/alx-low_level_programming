debcf1edb2b70619484ac8ed53bd631e090dbc03
Viv <kendivivianne@gmail.com>
2022-10-18 13:45:05 -0700

    multiplies two positive numbers

---
+++ 0x0C-more_malloc_free/101-mul.c
@@ -0,0 +1,11 @@
+#include "main.h"
+#include <stdlib.h>
+#include <stdio.h>
+/**
+ * str_len - finds string length
+ * @str: input pointer to string
+ * Return: length of string
+ */
+int str_len(char *str)
+{
+}
