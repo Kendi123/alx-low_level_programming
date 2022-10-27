73e1a35b0ac64107040bb8464f765c2f9adb7782
Viv <kendivivianne@gmail.com>
2022-10-26 22:42:50 -0700

    Updates to lists

--- 0x13-more_singly_linked_lists/lists.h
+++ 0x13-more_singly_linked_lists/lists.h
@@ -7,18 +7,16 @@
 
 /**
  * struct list_s - singly linked list
- * @str: string - (malloc'ed string)
- * @len: length of the string
+ * @n: integer
  * @next: points to the next node
  *
  * Description: singly linked list node structure
  */
-typedef struct list_s
+typedef struct listint_s
 {
-	char *str;
-	unsigned int len;
-	struct list_s *next;
-} list_t;
+	int n;
+	struct listint_s *next;
+} listint_t;
 
 size_t print_listint(const listint_t *h);
 size_t listint_len(const listint_t *h);
@@ -31,3 +29,5 @@ listint_t *get_nodeint_at_index(listint_t *head, unsigned int index);
 int sum_listint(listint_t *head);
 listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n);
 int delete_nodeint_at_index(listint_t **head, unsigned int index);
+
+#endif
